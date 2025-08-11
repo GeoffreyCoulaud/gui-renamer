5fbd99cb0931f5a67e09af11aa8ee8c6efa2fb9f
Geoffrey Coulaud <geoffrey.coulaud@pictarine.com>
2025-08-11 08:42:46 +0200

    Change model to a gobject

--- src/main/models/main_model.py
+++ src/main/models/main_model.py
@@ -1,9 +1,20 @@
-class MainModel:
+from enum import StrEnum
+
+from gi.repository import GObject  # type: ignore
+
+
+class MainModel(GObject.Object):
     """MVC model for the main application logic."""
 
-    def __init__(self) -> None:
-        self.picked_file_paths: list[str] = []
-        self.renamed_file_paths: list[str] = []
-        self.regex: str = ""
-        self.replace_pattern: str = ""
-        self.apply_to_full_path: bool = False
+    class Signals(StrEnum):
+        NOTIFY_PICKED_FILE_PATHS = "notify::picked-file-paths"
+        NOTIFY_RENAMED_FILE_PATHS = "notify::renamed-file-paths"
+        NOTIFY_REGEX = "notify::regex"
+        NOTIFY_REPLACE_PATTERN = "notify::replace-pattern"
+        NOTIFY_APPLY_TO_FULL_PATH = "notify::apply-to-full-path"
+
+    picked_file_paths: list[str] = GObject.Property(type=object)
+    renamed_file_paths: list[str] = GObject.Property(type=object)
+    regex: str = GObject.Property(type=str, default="")
+    replace_pattern: str = GObject.Property(type=str, default="")
+    apply_to_full_path: bool = GObject.Property(type=bool, default=False)
